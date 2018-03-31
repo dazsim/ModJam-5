be4e056eb17b77d7169ffe941059dc444f53efe2
Steven Blom <atomicblom@binaryvibrance.net>
2018-03-31 23:17:05 +1030

    Read instruments from world

--- src/main/java/org/atomicworkshop/tiles/TileEntitySequencer.java
+++ src/main/java/org/atomicworkshop/tiles/TileEntitySequencer.java
@@ -1,11 +1,16 @@
 package org.atomicworkshop.tiles;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.world.NoteBlockEvent.Instrument;
 import org.atomicworkshop.Reference.NBT;
 import org.atomicworkshop.sequencing.MusicPlayer;
@@ -15,17 +20,19 @@ import org.atomicworkshop.sequencing.SequencerSet;
 import javax.annotation.Nullable;
 import java.util.UUID;
 
-public class TileEntitySequencer extends TileEntity
+import static net.minecraftforge.event.world.NoteBlockEvent.Instrument.*;
+
+public class TileEntitySequencer extends TileEntity implements ITickable
 {
 
 	public TileEntitySequencer()
 	{
-		demoSongUUID = UUID.randomUUID();
+		sequencerSetId = UUID.randomUUID();
 	}
 
-	private UUID demoSongUUID;
+	private UUID sequencerSetId;
 
-	private Sequencer sequencer;
+	private Sequencer sequencer = null;
 	private boolean isPlaying;
 
 	private boolean hasSynchronizer()
@@ -45,9 +52,9 @@ public class TileEntitySequencer extends TileEntity
 		super.readFromNBT(compound);
 		isPlaying = compound.getBoolean(NBT.isPlaying);
 
-		demoSongUUID = compound.getUniqueId(NBT.songId);
-		if (demoSongUUID.equals(UUID.fromString("00000000-0000-0000-0000-000000000000"))) {
-			demoSongUUID = UUID.randomUUID();
+		sequencerSetId = compound.getUniqueId(NBT.songId);
+		if (sequencerSetId.equals(UUID.fromString("00000000-0000-0000-0000-000000000000"))) {
+			sequencerSetId = UUID.randomUUID();
 		}
 	}
 
@@ -57,7 +64,7 @@ public class TileEntitySequencer extends TileEntity
 		super.writeToNBT(compound);
 
 		compound.setBoolean(NBT.isPlaying, isPlaying);
-		compound.setUniqueId(NBT.songId, demoSongUUID);
+		compound.setUniqueId(NBT.songId, sequencerSetId);
 
 		return compound;
 	}
@@ -66,7 +73,7 @@ public class TileEntitySequencer extends TileEntity
 	@Override
 	public SPacketUpdateTileEntity getUpdatePacket()
 	{
-		NBTTagCompound updateTag = getUpdateTag();
+		final NBTTagCompound updateTag = getUpdateTag();
 		return new SPacketUpdateTileEntity(pos, 0, updateTag);
 	}
 
@@ -80,21 +87,47 @@ public class TileEntitySequencer extends TileEntity
 	public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt)
 	{
 		super.onDataPacket(net, pkt);
-		boolean wasPlaying = isPlaying;
+		final boolean wasPlaying = isPlaying;
 		handleUpdateTag(pkt.getNbtCompound());
 
 		if (isPlaying && !wasPlaying) {
-			createDemoSong();
-
+			startPlaying();
+			sendUpdates();
 		} else if (!isPlaying && wasPlaying) {
-			MusicPlayer.stopPlaying(new SequencerSet(world, demoSongUUID));
+			stopPlaying();
 			sendUpdates();
 		}
 	}
 
+	private void startPlaying()
+	{
+		//TODO: Only create a demo song if there isn't any pattern data attached to this TE.
+		createDemoSong();
+
+		//TODO: Find Synchronizer and get the Sequencer set for it.
+		final SequencerSet demoSong;
+
+		if (!hasSynchronizer()) {
+			demoSong = new SequencerSet(world, sequencerSetId);
+		} else {
+			//TODO: Resolve SequencerSet from sequencer
+			demoSong = new SequencerSet(world, sequencerSetId);
+		}
+		demoSong.addSequencer(sequencer);
+
+		demoSong.updateBpm();
+
+		for (final EnumFacing horizontal : EnumFacing.HORIZONTALS)
+		{
+			verifyNoteBlockFacing(horizontal);
+		}
+
+		MusicPlayer.playSong(demoSong);
+	}
+
 	private void sendUpdates() {
 		world.markBlockRangeForRenderUpdate(pos, pos);
-		IBlockState state = world.getBlockState(pos);
+		final IBlockState state = world.getBlockState(pos);
 		world.notifyBlockUpdate(pos, state, state, 3);
 		world.scheduleBlockUpdate(pos, getBlockType(),0,0);
 		markDirty();
@@ -102,7 +135,7 @@ public class TileEntitySequencer extends TileEntity
 
 	public void stopPlaying()
 	{
-		MusicPlayer.stopPlaying(new SequencerSet(world, demoSongUUID));
+		MusicPlayer.stopPlaying(new SequencerSet(world, sequencerSetId));
 	}
 
 	public void notifyPowered(boolean powered)
@@ -117,26 +150,17 @@ public class TileEntitySequencer extends TileEntity
 	private void createDemoSong()
 	{
 		if (world == null) return;
-		//TODO: Find Synchronizer and get the Sequencer set for it.
-		final SequencerSet demoSong;
 
-		if (!hasSynchronizer()) {
-			demoSong = new SequencerSet(world, demoSongUUID);
-		} else {
-			//TODO: Resolve SequencerSet from sequencer
-			demoSong = new SequencerSet(world, demoSongUUID);
-		}
-
-		sequencer = demoSong.addSequencer(pos);
+		sequencer = new Sequencer(world, pos);
 		sequencer.setDesiredBPM(120);
 
-		int pattern = world.rand.nextInt(4);
+		final int pattern = world.rand.nextInt(4);
 		final Pattern demoPattern = new Pattern();
 		if (pattern == 0)
 		{
-			sequencer.setAdjacentNoteBlock(EnumFacing.NORTH, Instrument.PIANO);
-			sequencer.setAdjacentNoteBlock(EnumFacing.SOUTH, Instrument.BASSGUITAR);
-			sequencer.setAdjacentNoteBlock(EnumFacing.EAST, Instrument.PIANO);
+//			sequencer.setAdjacentNoteBlock(EnumFacing.NORTH, PIANO);
+//			sequencer.setAdjacentNoteBlock(EnumFacing.SOUTH, BASSGUITAR);
+//			sequencer.setAdjacentNoteBlock(EnumFacing.EAST, PIANO);
 
 			demoPattern.setPitchAtInternal(0, 6);
 			demoPattern.setPitchAtInternal(1, 10);
@@ -155,18 +179,18 @@ public class TileEntitySequencer extends TileEntity
 			demoPattern.setPitchAtInternal(14, 18);
 
 		} else if (pattern == 1) {
-			sequencer.setAdjacentNoteBlock(EnumFacing.NORTH, Instrument.BASSDRUM);
+//			sequencer.setAdjacentNoteBlock(EnumFacing.NORTH, BASSDRUM);
 
 			demoPattern.setPitchAtInternal(0, 6);
 			demoPattern.setPitchAtInternal(8, 6);
 
 		} else if (pattern == 2) {
-			sequencer.setAdjacentNoteBlock(EnumFacing.NORTH, Instrument.SNARE);
+//			sequencer.setAdjacentNoteBlock(EnumFacing.NORTH, Instrument.SNARE);
 
 			demoPattern.setPitchAtInternal(4, 6);
 			demoPattern.setPitchAtInternal(12, 6);
 		} else if (pattern == 3) {
-			sequencer.setAdjacentNoteBlock(EnumFacing.NORTH, Instrument.CLICKS);
+//			sequencer.setAdjacentNoteBlock(EnumFacing.NORTH, Instrument.CLICKS);
 
 			demoPattern.setPitchAtInternal(1, 14);
 			demoPattern.setPitchAtInternal(2, 18);
@@ -186,9 +210,90 @@ public class TileEntitySequencer extends TileEntity
 		}
 
 		sequencer.setPattern(0, demoPattern);
+	}
 
-		demoSong.updateBpm();
 
-		MusicPlayer.playSong(demoSong);
+	@Override
+	public void update()
+	{
+		if (sequencer == null) return;
+
+		final int blockToCheck = (int)(world.getTotalWorldTime() & 3);
+		final EnumFacing facing = EnumFacing.getHorizontal(blockToCheck);
+
+		verifyNoteBlockFacing(facing);
+	}
+
+	private void verifyNoteBlockFacing(EnumFacing facing)
+	{
+		BlockPos offset = pos.offset(facing.getOpposite());
+		final IBlockState noteBlockState = world.getBlockState(offset);
+		final Instrument instrument;
+		if (noteBlockState.getBlock() != Blocks.NOTEBLOCK) {
+			instrument = null;
+		} else {
+			final IBlockState instrumentBlockState = world.getBlockState(offset.down());
+			instrument = getInstrumentFromBlockState(instrumentBlockState);
+		}
+		final Instrument instrumentFromNoteBlock = sequencer.getInstrumentFromNoteBlock(facing);
+
+		if (instrument != instrumentFromNoteBlock) {
+			sequencer.setAdjacentNoteBlock(facing, instrument);
+			sendUpdates();
+		}
+	}
+
+	private static Instrument getInstrumentFromBlockState(IBlockState state)
+	{
+		//Blatantly ripped from TileEntityNote
+		final Material material = state.getMaterial();
+
+		if (material == Material.ROCK)
+		{
+			return BASSDRUM;
+		}
+
+		if (material == Material.SAND)
+		{
+			return SNARE;
+		}
+
+		if (material == Material.GLASS)
+		{
+			return CLICKS;
+		}
+
+		if (material == Material.WOOD)
+		{
+			return BASSGUITAR;
+		}
+
+		final Block block = state.getBlock();
+
+		if (block == Blocks.CLAY)
+		{
+			return FLUTE;
+		}
+
+		if (block == Blocks.GOLD_BLOCK)
+		{
+			return BELL;
+		}
+
+		if (block == Blocks.WOOL)
+		{
+			return GUITAR;
+		}
+
+		if (block == Blocks.PACKED_ICE)
+		{
+			return CHIME;
+		}
+
+		if (block == Blocks.BONE_BLOCK)
+		{
+			return XYLOPHONE;
+		}
+		return PIANO;
 	}
 }
