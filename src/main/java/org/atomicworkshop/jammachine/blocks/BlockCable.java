8f01267f4968c2f1b2994dc345fce1a8de913020
workshopcraft <darrensim42@gmail.com>
2018-04-08 11:06:17 +0100

    Broken Properties Checkin

--- src/main/java/org/atomicworkshop/jammachine/blocks/BlockCable.java
+++ src/main/java/org/atomicworkshop/jammachine/blocks/BlockCable.java
@@ -3,6 +3,7 @@ package org.atomicworkshop.jammachine.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
@@ -28,12 +29,32 @@ import javax.annotation.Nullable;
 @SuppressWarnings("deprecation")
 public class BlockCable extends Block 
 {
-	private final AxisAlignedBB boundingBox = new AxisAlignedBB(0, 0, 0, 1, 0.5, 1);
+	
+	/* cable placed on floor */
+	 public static final PropertyBool FLOOR = PropertyBool.create("floor");
+	 /* cable placed on floor */
+	 public static final PropertyBool CEILING = PropertyBool.create("ceiling");
+	 /* cable placed on floor */
+	 
+	 //public static final PropertyBool NORTH = PropertyBool.create("north");
+	 /* cable placed on floor */
+	 //public static final PropertyBool SOUTH = PropertyBool.create("south");
+	 /* cable placed on floor */
+	 //public static final PropertyBool EAST = PropertyBool.create("east");
+	 /* cable placed on floor */
+	 //public static final PropertyBool WEST = PropertyBool.create("west");
+	 
+	 
+	 
+	private final AxisAlignedBB boundingBox = new AxisAlignedBB(0, 0, 0, 1, 0.2, 1);
 
 	public BlockCable() {
 		super(new MachineMaterial());
-
-		final IBlockState defaultState = blockState.getBaseState();
+		//blockState.validateProperty(block, property)
+		//final IBlockState defaultState = blockState.getBaseState().withProperty(NORTH, Boolean.FALSE).withProperty(EAST, Boolean.FALSE).withProperty(SOUTH, Boolean.FALSE).withProperty(WEST, Boolean.FALSE).withProperty(FLOOR, Boolean.FALSE).withProperty(CEILING, Boolean.FALSE);
+		
+		final IBlockState defaultState = this.blockState.getBaseState().withProperty(FLOOR, Boolean.FALSE).withProperty(CEILING, Boolean.FALSE);
+		
 		setDefaultState(defaultState);
 	}
 
@@ -61,7 +82,7 @@ public class BlockCable extends Block
 	@Override
 	public int getMetaFromState(IBlockState state)
 	{
-		return 0; //TODO : this will change as i add support for cables connecting to each other
+		return 0; //TODO : this will change as i add support for cables connecting to each other. or not
 	}
 
 	@Override
@@ -76,6 +97,39 @@ public class BlockCable extends Block
 	                                        float hitX, float hitY, float hitZ, int meta,
 	                                        EntityLivingBase placer, EnumHand hand)
 	{
+		/*check if there is already a cable in this block*/
+		if (world.getBlockState(pos).getBlock() instanceof BlockCable)
+		{
+			/*check if there is already a cable on this surface*/
+			if (facing.equals(facing.DOWN))
+			{
+				if (world.getBlockState(pos).getProperties().containsKey(FLOOR))
+				{
+					return (world.getBlockState(pos));
+				}  else
+				{
+					return (world.getBlockState(pos).withProperty(this.FLOOR, true));
+				}
+			}
+			if (facing.equals(facing.UP))
+			{
+				if (world.getBlockState(pos).getProperties().containsKey(CEILING))
+				{
+					return (world.getBlockState(pos));
+				} else
+				{
+					/*valid placement. make sure there is a valid block above this one*/
+					if (world.getBlockState(pos).getBlock().isFullBlock(world.getBlockState(pos)))
+					{
+						return (world.getBlockState(pos).withProperty(this.CEILING, true));
+					}
+				}
+			}
+		}
+		if (facing.DOWN.equals(facing))
+		{
+			return getDefaultState().withProperty(this.FLOOR, true);
+		}
 		return getDefaultState(); //TODO this will change as i add support for cables connecting when you place them
 	}
 /*
@@ -124,9 +178,17 @@ public class BlockCable extends Block
 	@Deprecated
 	public boolean isOpaqueCube(IBlockState state)
 	{
-		return false;
+		return false; 
 	}
-
+	@Override
+	public boolean isTopSolid(IBlockState state)
+	{
+		return true;
+		//return false; 
+	}
+	
+	
+	
 	@Override
 	@Deprecated
 	public boolean eventReceived(IBlockState state, World world, BlockPos pos, int id, int param)
