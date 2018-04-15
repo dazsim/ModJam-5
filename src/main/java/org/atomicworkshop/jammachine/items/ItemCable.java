7522ea3e9c45db03204dc127605ea97d4af88471
workshopcraft <darrensim42@gmail.com>
2018-04-15 02:32:38 +0100

    finished internal cable connection models.
    next job, connection to adjcent cables and cables around an external bend

--- src/main/java/org/atomicworkshop/jammachine/items/ItemCable.java
+++ src/main/java/org/atomicworkshop/jammachine/items/ItemCable.java
@@ -1,6 +1,7 @@
 package org.atomicworkshop.jammachine.items;
 
 import org.atomicworkshop.jammachine.JamMachineMod;
+import org.atomicworkshop.jammachine.blocks.BlockCable;
 import org.atomicworkshop.jammachine.libraries.BlockLibrary;
 import org.atomicworkshop.jammachine.tiles.TileEntityCable;
 
@@ -26,9 +27,35 @@ public class ItemCable extends Item{
 			float hitY, float hitZ) {
 		EnumActionResult e = super.onItemUse(player, world, pos, hand,side, hitX, hitY, hitZ);
 		
-		
+			
 		
 			JamMachineMod.logger.info(world.getBlockState(pos.offset(side)).getBlock().toString());
+			if (world.getBlockState(pos).getBlock() instanceof BlockCable)
+			{
+				//we are clicking an existing block
+				if (world.getBlockState(pos.offset(side.getOpposite())).getBlock().isSideSolid(world.getBlockState(pos.offset(side.getOpposite())), world, pos, side))
+				{
+					//is block placing on solid
+					JamMachineMod.logger.info("Mod BLOCK"+side.toString());
+					TileEntity t = world.getTileEntity(pos);
+					TileEntityCable t2 = (TileEntityCable)t;
+					logTE(t2);
+					//NORTH
+					
+						
+						if (!t2.hasCable(side.getOpposite()))
+						{
+							t2.setCable(side.getOpposite(), true);
+							//world.setBlockState(pos, BlockLibrary.cable.getActualState(null, world, pos), 3);
+							world.markAndNotifyBlock(pos, null, world.getBlockState(pos), BlockLibrary.cable.getActualState(null,world,pos), 3);
+							JamMachineMod.logger.info("setting block"+side.toString());
+							logTE(t2);
+							return e.SUCCESS;
+						}
+					
+					
+				}
+			}
 			if (world.getBlockState(pos.offset(side)).getBlock() instanceof BlockAir)
 			{
 				if (side.equals(EnumFacing.UP))
@@ -59,6 +86,66 @@ public class ItemCable extends Item{
 					logTE(t2);
 					return e.SUCCESS;
 				}
+				else
+				if (side.equals(EnumFacing.NORTH))
+				{
+					//this should create a new cable at coordinates
+					JamMachineMod.logger.info("Place BLOCK");
+					world.setBlockState(pos.offset(side), BlockLibrary.cable.getDefaultState().withProperty(BlockLibrary.cable.NORTH,Boolean.TRUE), 3);
+					
+					world.markAndNotifyBlock(pos.offset(side), null, world.getBlockState(pos.offset(side)), BlockLibrary.cable.getDefaultState().withProperty(BlockLibrary.cable.NORTH,Boolean.TRUE), 3);
+					TileEntity t = world.getTileEntity(pos.offset(side));
+					TileEntityCable t2 = (TileEntityCable)t;
+					t2.setCable(side.getOpposite(), true);
+					
+					logTE(t2);
+					return e.SUCCESS;
+				}
+				else
+					if (side.equals(EnumFacing.SOUTH))
+					{
+						//this should create a new cable at coordinates
+						JamMachineMod.logger.info("Place BLOCK");
+						world.setBlockState(pos.offset(side), BlockLibrary.cable.getDefaultState().withProperty(BlockLibrary.cable.SOUTH,Boolean.TRUE), 3);
+						
+						world.markAndNotifyBlock(pos.offset(side), null, world.getBlockState(pos.offset(side)), BlockLibrary.cable.getDefaultState().withProperty(BlockLibrary.cable.SOUTH,Boolean.TRUE), 3);
+						TileEntity t = world.getTileEntity(pos.offset(side));
+						TileEntityCable t2 = (TileEntityCable)t;
+						t2.setCable(side.getOpposite(), true);
+						
+						logTE(t2);
+						return e.SUCCESS;
+					}
+					else
+					if (side.equals(EnumFacing.EAST))
+					{
+						//this should create a new cable at coordinates
+						JamMachineMod.logger.info("Place BLOCK");
+						world.setBlockState(pos.offset(side), BlockLibrary.cable.getDefaultState().withProperty(BlockLibrary.cable.EAST,Boolean.TRUE), 3);
+						
+						world.markAndNotifyBlock(pos.offset(side), null, world.getBlockState(pos.offset(side)), BlockLibrary.cable.getDefaultState().withProperty(BlockLibrary.cable.EAST,Boolean.TRUE), 3);
+						TileEntity t = world.getTileEntity(pos.offset(side));
+						TileEntityCable t2 = (TileEntityCable)t;
+						t2.setCable(side.getOpposite(), true);
+						
+						logTE(t2);
+						return e.SUCCESS;
+					}
+					else
+						if (side.equals(EnumFacing.WEST))
+						{
+							//this should create a new cable at coordinates
+							JamMachineMod.logger.info("Place BLOCK");
+							world.setBlockState(pos.offset(side), BlockLibrary.cable.getDefaultState().withProperty(BlockLibrary.cable.WEST,Boolean.TRUE), 3);
+							
+							world.markAndNotifyBlock(pos.offset(side), null, world.getBlockState(pos.offset(side)), BlockLibrary.cable.getDefaultState().withProperty(BlockLibrary.cable.WEST,Boolean.TRUE), 3);
+							TileEntity t = world.getTileEntity(pos.offset(side));
+							TileEntityCable t2 = (TileEntityCable)t;
+							t2.setCable(side.getOpposite(), true);
+							
+							logTE(t2);
+							return e.SUCCESS;
+						}
 			}
 		
 		return e.FAIL;
